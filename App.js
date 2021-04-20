a2d9a8c196a26a529316cfc1e345324fc650f99c
Alex Rueb <rueba@oregonstate.edu>
2021-04-19 19:56:56 -0500

    added modal pop up to AppPicker component

--- App.js
+++ App.js
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import Screen from "./app/components/Screen";
 import AppTextInput from "./app/components/AppTextInput";
 import { Switch } from "react-native";
+import AppPicker from "./app/components/AppPicker";
 
 export default function App() {
   const [isNew, setIsNew] = useState(false);
 
   return (
     <Screen>
-      <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
+      <AppPicker icon="apps" placeholder="Category" />
+      <AppTextInput icon="email" placeholder="Email" />
+      {/* <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} /> */}
     </Screen>
   );
 }
---
+++ app/components/AppPicker.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Platform,
+  TouchableWithoutFeedback,
+  Modal,
+  Button,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import Screen from "./Screen";
+import defaultStyles from "../config/styles";
+import AppText from "./AppText";
+
+function AppPicker({ icon, placeholder, ...otherProps }) {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  return (
+    <React.Fragment>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons
+              name={icon}
+              size={20}
+              color={defaultStyles.colors.medium}
+              style={styles.icon}
+            />
+          )}
+          <AppText style={styles.text}>{placeholder}</AppText>
+          <MaterialCommunityIcons
+            name="chevron-down"
+            size={20}
+            color={defaultStyles.colors.medium}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType="slide">
+        <Screen>
+          <Button title="Close" onPress={() => setModalVisible(false)} />
+        </Screen>
+      </Modal>
+    </React.Fragment>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: defaultStyles.colors.light,
+    borderRadius: 25,
+    flexDirection: "row",
+    marginVertical: 10,
+    width: "100%",
+    padding: 15,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  text: {
+    flex: 1,
+  },
+});
+
+export default AppPicker;
