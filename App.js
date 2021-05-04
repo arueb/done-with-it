1c9e0f9a98bd0e2afb181801f97db2c1f1140652
Alex Rueb <rueba@oregonstate.edu>
2021-05-04 07:43:05 -0500

    added bottomTabNavigator

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Button, Text } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Screen from "./app/components/Screen";
 
 const Link = () => {
   const navigation = useNavigation();
-  return (
-    <Button title="Click" onPress={() => navigation.navigate("TweetDetails")} />
-  );
+  return <Button title="Click" onPress={() => navigation.navigate("Tweets")} />;
 };
 
 const Tweets = ({ navigation }) => (
@@ -22,22 +22,34 @@ const Tweets = ({ navigation }) => (
         })
       }
     />
-    <Link />
   </Screen>
 );
 const TweetDetails = ({ route }) => (
   <Screen>
     <Text>Tweet Details {route.params.id}</Text>
+    <Link />
   </Screen>
 );
 
 const Stack = createStackNavigator();
 const StackNavigator = () => (
-  <Stack.Navigator>
+  <Stack.Navigator
+    screenOptions={{
+      // global navigation styles
+      title: "Tweet Details",
+      headerStyle: { backgroundColor: "dodgerblue" },
+      headerTintColor: "white",
+    }}
+  >
     <Stack.Screen
       name="Tweets"
       component={Tweets}
-      options={{ title: "Tweet Details" }} // pass object for static title
+      options={{
+        // applies only to this screen
+        title: "Tweet Details",
+        headerStyle: { backgroundColor: "tomato" },
+        // headerShown: false,
+      }} // pass object for static title
     />
     <Stack.Screen
       name="TweetDetails"
@@ -46,10 +58,40 @@ const StackNavigator = () => (
     />
   </Stack.Navigator>
 );
+const Account = () => (
+  <Screen>
+    <Text>Account</Text>
+  </Screen>
+);
+
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator
+    tabBarOptions={{
+      activeBackgroundColor: "tomato",
+      activeTintColor: "white",
+      inactiveBackgroundColor: "lightgrey",
+      inactiveTintColor: "black",
+    }}
+  >
+    <Tab.Screen
+      name="Feed"
+      component={Tweets}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons name="home" size={size} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen name="Account" component={Account} />
+  </Tab.Navigator>
+);
+
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <TabNavigator />
     </NavigationContainer>
   );
 }
