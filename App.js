27ca503b02f3bf972e0755ecb764dc665cf5cd5d
Alex Rueb <rueba@oregonstate.edu>
2021-05-12 23:24:43 -0500

    AsyncStorage package install and testing

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import NetInfo from "@react-native-community/netinfo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 
 export default function App() {
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 })); // save a key/value pair
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.parse(value);
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  demo();
+
   return (
     <NavigationContainer theme={navigationTheme}>
       <AppNavigator />
